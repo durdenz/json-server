9460a135c5e08d036eee116f4abe8e86103241ca
George Durden <george@durden.com>
2024-08-28 12:21:51 -0400

    Cleanup JS

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
 
-const middlewares = jsonServer.defaults()
+const mw = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(mw);
+server.use(router);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log('JSON server is running on port $(port)')
-})
+    console.log('JSON server is running on port $(port)');
+});
